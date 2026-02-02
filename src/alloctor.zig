a119304286c5cc82e65ef6e8cfa922826da1ea71
Onyedika Benson <nwankwobenson29@gmail.com>
2026-02-02 06:44:34 +0100

    feat: Refactored the slab object to use a bitmap instead of an array

--- src/alloctor.zig
+++ src/alloctor.zig
@@ -2,24 +2,29 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 
 pub const Slab = struct {
-    bitmap: []bool,
+    bitmap: []u8,
     start_pos: usize,
     end_pos:usize,
     region_size:usize,
     region_count:usize,
 
-    pub fn init(allocator:Allocator,start_index: usize, end_index: usize, region_size:usize) Slab {
+    pub fn init(arena: []u8 ,start_index: usize, end_index: usize, region_size:usize) Slab {
         
         const region_count = (end_index - start_index)/region_size;
-        const bitmap = try allocator.alloc(bool, region_count);
-        @memset(bitmap, false);
+        const bitmap_size = (region_count + 7)/8;
+
+        const bitmap = arena[start_index..start_index + bitmap_size];
+        const start_pos = start_index + bitmap_size;
+
+        const usable_space = end_index - start_pos;
+        const usable_regions = usable_space / region_size;
+        @memset(bitmap, 0);
 
 
         return Slab{
-            .allocator = allocator,
-            .start_pos = start_index,
+            .start_pos = start_pos,
             .end_pos = end_index,
-            .region_count = region_count,
+            .region_count = usable_regions,
             .region_size = region_size,
             .bitmap = bitmap,
         };
@@ -29,6 +34,25 @@ pub const Slab = struct {
     pub fn deinit(self: *Slab,allocator: Allocator) !void {
         try allocator.free(self.bitmap);
     }
+
+    fn isUsed(self: *Slab, index: usize)bool{
+        const byte_index = index / 8;
+        const bit_index = index % 8;
+        return(self.bitmap[byte_index] & (1 << bit_index)) != 0;
+    }
+    fn setUsed(self: *Slab, index: usize)void{
+        const byte_index = index / 8;
+        const bit_index = index % 8;
+        self.bitmap[byte_index] |= 1 << bit_index;
+    }
+    fn clearUsed(self: *Slab, index: usize)void{
+        const byte_index = index / 8;
+        const bit_index = index % 8;
+        self.bitmap[byte_index] &= ~(1 << bit_index);
+    }
+
+
+
 
     pub fn alloc(self: *Slab, size: usize) !usize {
         const regions_needed = (size + self.region_size - 1)/self.region_size;
@@ -55,20 +79,6 @@ pub const Slab = struct {
         return error.OutOfMemory;
     }
 
-    pub fn free(self: *Slab, index: usize, size:usize)void{
-        const start_idx = (index - self.start_pos)/self.region_size;
-        const regions_to_free = (size + self.region_size - 1 )/self.region_size;
-        for(start_idx..start_idx + regions_to_free)|i|{
-            self.bitmap[i] = false;
-        }
-    }
-
-    pub fn isEmpty(self: *Slab) bool {
-    for (self.bitmap) |used| {
-        if (used) return false;
-    }
-    return true;
-    }
 
 
 };
